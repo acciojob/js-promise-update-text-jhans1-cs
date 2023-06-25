95f9514e52b82eacb3d5d0e0f8835eacedf18ecc
Jhansi <95856797+jhans1-cs@users.noreply.github.com>
2023-06-25 19:10:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+ function delayedGreeting() {
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            resolve("Hello, world!");
+          }, 1000);
+        });
+      }
+
+      delayedGreeting().then((message) => {
+        const outputElement = document.getElementById("output");
+        outputElement.textContent = message;
+      });
